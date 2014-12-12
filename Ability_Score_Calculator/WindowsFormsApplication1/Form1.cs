98c49f8905a2a3859fc2d14d7e0549a9b4d8495c
Grosswood <mozge4ok@gmail.com>
2014-12-12 12:31:09 +0700

    Few ideas for design
    Signed-off-by: Grosswood <mozge4ok@gmail.com>

--- Ability_Score_Calculator/WindowsFormsApplication1/Form1.cs
+++ Ability_Score_Calculator/WindowsFormsApplication1/Form1.cs
@@ -12,9 +12,29 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        public int pointsLeft = 27;
+        public int str = 8;
+
         public Form1()
         {
             InitializeComponent();
+            this.label1.Text = pointsLeft.ToString();
+            this.label3.Text = str.ToString();
+            this.label4.Text = "-1";
+            this.label5.Text = "1";
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = "Time " + DateTime.Now.ToLongTimeString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
     }
 }
